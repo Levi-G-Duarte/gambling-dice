417e382e527d7fbd0c852857ec1437e71e903c5a
Levi Duarte <levigduarte5@gmail.com>
2024-08-25 10:05:41 -0700

    the gambit has arrived 8/25

--- script.js
+++ script.js
@@ -10,33 +10,28 @@ function rollDice() {
 
     for (let i = 0; i < numOfDice; i++){
         const value = Math.floor(Math.random()*6) + 1;
+        cl(diceResult)
         values.push(value);
-        // cl(value);
         images.push(`<img src ="dice-images/${value}.jpg">`);
     }
-    diceResult.textContent = `dice: ${values.join(", ")}`;
+    diceResult.textContent = `${values.join(", ")}`;
     diceImages.innerHTML = images.join('');
-    if (diceResult == "6, 6, 6") {
-        cl(diceResult)
-        gameOutcome.textContent = "You've Won"
+
+    const valueNum = values.join(", ");
+
+    if (valueNum === "2") {
+        // cl(diceResult)
+        gameOutcome.textContent = "You've Won";
     }
-    if (diceResult == "5, 5, 5") {
-        cl(diceResult)
-        gameOutcome.textContent = "You've Won"
+    else if(valueNum === "1, 1") {
+        // cl(diceResult)
+        gameOutcome.textContent = "You've Won";
     }
-    if (diceResult === "4") {
-        cl(diceResult)
-        gameOutcome.textContent = "You've Won"
-    }
-    if (diceResult == "6, 6, 6") {
-        cl(diceResult)
-        gameOutcome.textContent = "You've Won"
-    }
-    if (diceResult == "6, 6, 6") {
-        cl(diceResult)
-        gameOutcome.textContent = "You've Won"
+    else if(valueNum === "3, 3, 3") {
+        // cl(diceResult)
+        gameOutcome.textContent = "You've Won";
     }
     else {
-        gameOutcome.textContent = "You lost"
+        gameOutcome.textContent = "You lost";
     }
 }
