471cd78604aaf8f37924042c7b9a58c2165a0e47
Levi Duarte <lduart441@west-mec.org>
2024-09-12 17:29:33 -0700

    more gambit logic

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 //Gambling Dice Program
 let cl = console.log;
-let  money = 100;
+let money = 100;
+// Bet Raises
+let lowBet = 5;
+let mediumBet = 10;
+let highBet = 20;
+let maxBet = 50;
 
 function rollDice() {
     const numOfDice = document.getElementById('numOfDice').value;
@@ -18,30 +23,35 @@ function rollDice() {
     }
     diceResult.textContent = `${values.join(", ")}`;
     diceImages.innerHTML = images.join('');
-
     const valueNum = values.join(", ");
-
+    ////////////////////////////////////////////////// SCORE CLASS LOGIC
     if (valueNum == 2) {
-        // cl(diceResult)
         gameOutcome.textContent = "You've Won";
         money += 10
         gameOutcome.style.color = "green";
+        gameOutcome = true
     }
     else if(valueNum === "1, 1") {
-        // cl(diceResult)
         money += 10
         gameOutcome.textContent = "You've Won";
+        gameOutcome = true
     }
     else if (valueNum === "1, 1, 1"|| valueNum === "2, 2, 2" || valueNum === "3, 3, 3" || valueNum === "4, 4, 4" || valueNum === "5, 5, 5," || valueNum === "6, 6, 6") {
-        // cl(diceResult)
         money += 10 
         gameOutcome.textContent = "You've Won";
-        valueNum === "1, 1, 1"
+        gameOutcome.style.color = "green";
+        gameOutcome = true
     }
     else {
         gameOutcome.textContent = "You lost";
         gameOutcome.style.color = "red";
+        gameOutcome = false
         money -= 10
     }
+
+
+    //////////////////////////////////////////////////// NUMDICE BET LOGIC
+
+
     cl(money)
 }
